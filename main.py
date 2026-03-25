9087e3ff2cdf7bc974940be3a9a47d5f01ae3d74
Sumanth Thota <sumanththota987@gmail.com>
2026-03-25 17:16:03 +0530

    Update 1212main.py

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ def get_shipments(next_token: str = None):
 
 # 🚀 FINAL: BATCH PAGINATION (NO TIMEOUT)
 @app.get("/getShipmentsBatch")
-def get_shipments_batch(max_pages: int = 3, next_token: str = None):
+def get_shipments_batch(max_pages: int = 1, next_token: str = None):
     try:
         access_token = get_access_token()
         auth = get_auth()
